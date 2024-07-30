35b5790415af17368b0e5e884832c83502dd13cf
Yury Fedotov <yury_fedotov@mckinsey.com>
2024-07-30 18:48:51 -0400

    Implement numeric `min_file_size`

--- src/cli.rs
+++ src/cli.rs
@@ -2,6 +2,7 @@ use clap::{Arg, Command};
 
 pub struct CLIArgs {
     pub directory: String,
+    pub min_file_size: usize,
     pub n_top: usize,
 }
 
@@ -16,6 +17,12 @@ pub fn parse_args() -> CLIArgs {
                 .index(1),
         )
         .arg(
+            Arg::new("min_file_size")
+                .help("Minimum file size to consider")
+                .long("min_file_size")
+                .default_value("3145728"),
+        )
+        .arg(
             Arg::new("n_top")
                 .help("Number of top files to return based on size")
                 .short('n')
@@ -28,10 +35,18 @@ pub fn parse_args() -> CLIArgs {
         .get_one::<String>("directory")
         .expect("Directory argument missing")
         .clone();
+    let min_file_size = matches
+        .get_one::<String>("min_file_size")
+        .map(|s| s.parse().unwrap_or(3145728))
+        .unwrap_or(3145728);
     let n_top = matches
         .get_one::<String>("n_top")
         .map(|s| s.parse().unwrap_or(5)) // Parse the value and default to 5 on error
         .unwrap_or(5);
 
-    CLIArgs { directory, n_top }
+    CLIArgs {
+        directory,
+        min_file_size,
+        n_top,
+    }
 }
