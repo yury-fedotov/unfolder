6ea527f0244fe1a879689957535a6e75e1c1dfce
Yury Fedotov <yury_fedotov@mckinsey.com>
2024-07-31 15:20:22 -0400

    Avoid duplication in file size thresholds

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod cli;
+mod file_sizes;
 mod file_utils;
 mod results;
 mod traversal;
--- src/results.rs
+++ src/results.rs
@@ -1,6 +1,7 @@
 use crate::file_utils::FileInfo;
 use crate::traversal::CompleteTraversalStatistics;
 
+use crate::file_sizes::format_size;
 use colored::*;
 use num_format::{Locale, ToFormattedString};
 
@@ -57,7 +58,7 @@ impl AnalysisResults {
             println!(
                 "{}: {}",
                 file.path.display().to_string().cyan(),
-                format_size(file.size).magenta()
+                format_size(file.size as usize).magenta()
             );
         }
 
@@ -69,22 +70,5 @@ impl AnalysisResults {
             }
             println!();
         }
-    }
-}
-
-/// Converts bytes into a human-readable format (bytes, KB, MB, GB)
-fn format_size(size: u64) -> String {
-    const KB: u64 = 1024;
-    const MB: u64 = KB * 1024;
-    const GB: u64 = MB * 1024;
-
-    if size >= GB {
-        format!("{:.2} GB", size as f64 / GB as f64)
-    } else if size >= MB {
-        format!("{:.2} MB", size as f64 / MB as f64)
-    } else if size >= KB {
-        format!("{:.2} KB", size as f64 / KB as f64)
-    } else {
-        format!("{} bytes", size)
     }
 }
