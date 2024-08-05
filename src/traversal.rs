c7b8fc46ba13be145d6eeb86c859b0d52d8a5f84
Yury Fedotov <yury_fedotov@mckinsey.com>
2024-08-05 13:26:39 -0400

    Implement parallel traversal

--- src/traversal.rs
+++ src/traversal.rs
@@ -2,6 +2,7 @@ use crate::file_utils;
 use crate::file_utils::FileInfo;
 use crate::file_utils::{calculate_hash, get_file_size};
 use ignore::WalkBuilder;
+use rayon::prelude::*;
 
 pub struct DirectoryTraversalOutput {
     pub complete_statistics: CompleteTraversalStatistics,
@@ -21,37 +22,34 @@ pub fn traverse_directory(
     min_file_size: &usize,
     file_extensions: &[String],
 ) -> DirectoryTraversalOutput {
-    let mut n_directories_visited = 1;
-    let mut max_depth_visited = 0;
-    let mut n_files_identified = 0;
-    let mut n_files_considered = 0;
-    let mut n_files_hashed = 0;
-    let file_infos: Vec<FileInfo> = WalkBuilder::new(dir)
+    let entries: Vec<_> = WalkBuilder::new(dir)
         .standard_filters(true)
         .build()
         .filter_map(|e| e.ok())
+        .collect();
+
+    let n_directories_visited = entries
+        .par_iter()
+        .filter(|e| e.file_type().map_or(false, |ft| ft.is_dir()))
+        .count();
+    let max_depth_visited = entries.par_iter().map(|e| e.depth()).max().unwrap_or(0);
+    let n_files_identified = entries
+        .par_iter()
+        .filter(|e| e.file_type().map_or(false, |ft| ft.is_file()))
+        .count();
+
+    let file_infos: Vec<FileInfo> = entries
+        .into_par_iter()
         .filter_map(|entry| {
-            let depth = entry.depth();
-            if depth > max_depth_visited {
-                max_depth_visited = depth;
-            }
-            if entry.file_type().map_or(false, |ft| ft.is_dir()) {
-                n_directories_visited += 1;
-            }
             if entry.file_type().map_or(false, |ft| ft.is_file()) {
-                n_files_identified += 1;
                 let path = entry.into_path();
                 if !file_extensions.is_empty()
                     && !file_utils::has_allowed_extension(&path, file_extensions)
                 {
                     return None;
                 }
-                n_files_considered += 1;
                 let size = get_file_size(&path);
                 let should_calculate_hash = size > *min_file_size as u64;
-                if should_calculate_hash {
-                    n_files_hashed += 1;
-                }
                 let hash = if should_calculate_hash {
                     calculate_hash(&path).unwrap_or_else(|_| String::new())
                 } else {
@@ -63,6 +61,12 @@ pub fn traverse_directory(
             }
         })
         .collect();
+
+    let n_files_considered = file_infos.len();
+    let n_files_hashed = file_infos
+        .iter()
+        .filter(|file_info| !file_info.hash.is_empty())
+        .count();
 
     let complete_statistics = CompleteTraversalStatistics {
         n_files_identified,
